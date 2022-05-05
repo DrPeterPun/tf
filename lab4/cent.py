71e16f8a35b281cc17ffb44b1efdb86045d4535f
DrPeterPun <pedro_pereira@outlook.pt>
2022-05-05 16:25:33 +0100

    lab4 done maybe I hope

---
+++ lab4/cent.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+import logging
+from asyncio import run, create_task, sleep
+
+from ams import send, receiveAll, reply
+from db import DB
+
+logging.getLogger().setLevel(logging.DEBUG)
+
+db = DB()
+#dict saving uncommited commands ( origianl msg id : (msg, originalnode ) ) 
+msgQ = {}
+# nr of the last commited msg, id is "id" + msgn
+msgn = 0
+
+#
+def numtoid(num):
+    return "id"+str(num)
+
+def idtonum(id):
+    return id[2:]
+
+#da commit a uma instrucao
+#se og=True, envia a mensagem de confirmacao ao
+async def commit(msg,db,og=False):
+    logging.info('commiting msg nr'+ str(msg.body.msg_id) )
+
+    ctx = await db.begin([k for op,k,v in msg.body.txn], msg.src+'-'+str(msg.body.msg_id))
+    rs,wv,res = await db.execute(ctx, msg.body.txn)
+    if res:
+        await db.commit(ctx, wv)
+        db.cleanup(ctx)
+        if(og):
+            reply(msg, type='txn_ok',txn=res)
+        return True
+    else:
+        if og:
+            reply(msg, type='error', code=14, text='transaction aborted')
+        logging.warning("transaction aborted by node" + node_id)
+        db.cleanup(ctx)
+        return False
+
+async def trycommits(msgQ,db):
+    global msgn
+    #check if the next msg exists in the Q
+    item = msgQ.pop(msgn+1,False)
+    #the msg exists, so we commit it
+    logging.info("cehcking if we can commit the msg: " + str(msgn+1))
+    logging.info("msgs" + str(msgQ))
+    if item:
+        msg=item[0]
+        og=item[1]
+        await commit(msg,db,og)
+        msgn+=1
+        await trycommits(msgQ,db)
+    else:
+        return
+
+async def handle(msg):
+    # State
+    global node_id, node_ids
+    global db
+
+    # Message handlers
+    if msg.body.type == 'init':
+        node_id = msg.body.node_id
+        node_ids = msg.body.node_ids
+        logging.info('node %s initialized', node_id)
+
+        reply(msg, type='init_ok')
+
+    elif msg.body.type == 'txn':
+        msgQ[msg.body.msg_id]= (msg,True)
+        #msgs de replicacao
+        for node in filter(lambda n: n!=node_id,node_ids):
+            logging.info("Sending msg to be replicated, dest" + node)
+            send(node_id, node, type='txnRep',msg=msg)
+        await trycommits(msgQ,db)
+
+    #replication msg or the commands
+    #ads command to q then tries to execute 
+    elif msg.body.type == 'txnRep':
+        #added msg to the Q
+        msgQ[msg.body.msg.body.msg_id]= (msg.body.msg,False)
+        await trycommits(msgQ,db)
+
+    else:
+        logging.warning('unknown message type %s', msg.body.type)
+
+# Main loop
+run(receiveAll(handle))
