9d0ba1816a436ce02f576637f23d4bb56db561e8
DrPeterPun <pedro_pereira@outlook.pt>
2022-05-18 17:06:09 +0100

    idk whu no works

--- lab4/cent.py
+++ lab4/cent.py
@@ -10,11 +10,15 @@ logging.getLogger().setLevel(logging.DEBUG)
 
 #master node
 
+#commands that still dont have an id, Queue
+queue = []
+
+#database manager
 db = DB()
 #dict saving uncommited commands ( origianl msg id : (msg, originalnode ) ) 
 msgQ = {}
 # nr of the last commited msg, id is "id" + msgn
-msgn = 0
+msgn = -1
 
 
 #da commit a uma instrucao
@@ -37,6 +41,7 @@ async def commit(msg,db,og=False):
         db.cleanup(ctx)
         return False
 
+# faz o maximo numero de commits possivel.
 async def trycommits(msgQ,db):
     global msgn
     #check if the next msg exists in the Q
@@ -57,34 +62,34 @@ async def handle(msg):
     # State
     global node_id, node_ids
     global db
-    global master
 
     # Message handlers
     if msg.body.type == 'init':
         node_id = msg.body.node_id
         node_ids = msg.body.node_ids
-        master = node_ids[0]
         logging.info('node %s initialized', node_id)
 
-        reply(msg, type='init_ok')
+        reply(msg, type='init_ok') 
 
     elif msg.body.type == 'txn':
-        #msgs de replicacao
-        #check if its master node
-        if not node_id==master:
-            send(msg.src,master,txn=msg.body.txn,type='txn')
-        #for node in filter(lambda n: n!=node_id,node_ids):
-        for node in node_ids:
-            logging.info("Sending msg to be replicated, dest" + node)
-            send(node_id, node, type='txnRep',msg=msg)
-        await trycommits(msgQ,db)
+        #asks for the msg number
+        send(node_id,'lin-tso',type='ts')
+        #adds the command to the queue
+        queue.append(msg)
 
     #replication msg or the commands
     #ads command to q then tries to execute 
     elif msg.body.type == 'txnRep':
         #added msg to the Q
-        msgQ[msg.body.msg.body.msg_id]= (msg.body.msg,node_id==msg.src)
+        msgQ[msg.body.ts] = (msg.body.msg,node_id==msg.src)
         await trycommits(msgQ,db)
+    
+    elif msg.body.type == 'ts_ok':
+        #for node in filter(lambda n: n!=node_id,node_ids)
+        m = queue.pop(0)
+        for node in node_ids:
+            logging.info("Sending msg to be replicated, to" + node)
+            send(node_id, node, type='txnRep',msg=m,ts=msg.body.ts)
 
     else:
         logging.warning('unknown message type %s', msg.body.type)
