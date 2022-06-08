d37c9ae7043b1f8ca99918436a81f8b3a009521c
DrPeterPun <pedro_pereira@outlook.pt>
2022-06-08 17:47:10 +0100

    final adjustments

--- lab4/cent.py
+++ lab4/cent.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import logging
-from asyncio import run, create_task, sleep
+from asyncio import run, create_task, sleep, Lock
 
 from ams import send, receiveAll, reply
 from db import DB
@@ -19,11 +19,21 @@ db = DB()
 msgQ = {}
 # nr of the last commited msg, id is "id" + msgn
 msgn = -1
+lock = Lock()
+
+def printState():
+    global msgQ,msgn,lock
+    logging.info('State check')
+    logging.info('lock:'+str(lock))
+    logging.info('msgQ:'+str(msgQ))
+    logging.info('msgn:'+str(msgn))
+    
 
 #executa e da commit a uma instrucao
 #se og=True, envia a mensagem de confirmacao ao cliente
-async def commit(msg,db,og=False):
-    logging.info('commiting msg nr'+ str(msg.body.msg_id) )
+async def commit(msg,db,ts,og=False):
+    logging.info('commiting msg nr'+ str(ts) )
+    logging.info('msg:'+ str(msg) )
 
     ctx = await db.begin([k for op,k,v in msg.body.txn], msg.src+'-'+str(msg.body.msg_id))
     rs,wv,res = await db.execute(ctx, msg.body.txn)
@@ -32,25 +42,31 @@ async def commit(msg,db,og=False):
         db.cleanup(ctx)
         if(og):
             reply(msg, type='txn_ok',txn=res)
+
+        #check if this command screws up the already executed commands
+        check_after(wv, msgQ,ts)
+        logging.info('sucessfully commited msg nr'+ str(ts) )
+        printState()
         return True
     else:
         if og:
             reply(msg, type='error', code=14, text='transaction aborted')
         logging.warning("transaction aborted by node" + node_id)
-        logging.warning("rs,wv,res"+ rs +"\\" + wv + "\\" +res)
         db.cleanup(ctx)
         return False
 
-async def only_commit(exec_result,db,og=False):
-    logging.info('commiting msg nr'+ str(msg.body.msg_id) )
+async def only_commit(exec_result,db,msg,ts,og=False):
+    logging.info('commiting a aleady processed message :' +str(ts) +', the results were: '+ str(exec_result) )
 
-    ctx = await db.begin([k for op,k,v in msg.body.txn], msg.src+'-'+str(msg.body.msg_id))
     rs,wv,res = exec_result
+    ctx = await db.begin([k for op,k,v in msg.body.txn], msg.src+'-'+str(msg.body.msg_id))
     if res:
         await db.commit(ctx, wv)
         db.cleanup(ctx)
         if(og):
             reply(msg, type='txn_ok',txn=res)
+        logging.info('sucessfully commited msg nr'+ str(ts) )
+        printState()
         return True
     else:
         #nao precisa de enviar a mensagem de erro porque ja foi enviada quando a execucao falhou
@@ -89,9 +105,9 @@ def check_before(msg,msgQ):
 
 #verifica se a execucao de um comando invalida a execucao dos comandos
 #posteriores
-def check_after(wv,msgQ):
+def check_after(wv,msgQ,ts):
     wk = wvtok(wv)
-    _,after=divide_dict(msg.body.ts,msgQ)
+    _,after=divide_dict(msgQ,ts)
     for n,msg in after.items():
         msgk=[k for op,k,v in msg.body.txn]
         # caso o comando futuro utilize de um valor que foi escrito pelo comando
@@ -111,16 +127,19 @@ async def execute(msg,db):
     if res:
         return (rs,wv,res)
     else:     
+        reply(msg, type='error', code=14, text='transaction aborted')
+        logging.warning("transaction aborted by node" + node_id)
         return False
 
 # faz o maximo numero de commits possivel.
 async def trycommits(msgQ,db):
-    global msgn
+    global msgn, lock
     #check if the next msg exists in the Q
+    await lock.acquire()
+    logging.info("checking if we can commit the msg: " + str(msgn+1))
+    logging.info("msgs" + str(msgQ))
     item = msgQ.pop(msgn+1,False)
     #the msg exists, so we commit it
-    logging.info("cehcking if we can commit the msg: " + str(msgn+1))
-    logging.info("msgs" + str(msgQ))
     if item:
         msg=item[0]
         og=item[1]
@@ -128,27 +147,34 @@ async def trycommits(msgQ,db):
         #execucao falhou, devolve erro
         if result==False:
             reply(msg, type='error', code=14, text='transaction aborted')
-            logging.warning("transaction aborted by node" + node_id)
-            (rs,wv,res) = result
-            logging.warning("rs,wv,res"+ rs +"\\" + wv + "\\" +res)
-            pass
+            logging.warning("transaction aborted by node " + node_id)
+            #(rs,wv,res) = result
+            #logging.warning("rs,wv,res"+ rs +"\\" + wv + "\\" +str(res))
+            #pass
         # nao foi executado ainda
         elif result==None:
-            await commit(msg,db,og)
+            logging.info("the message hasnt been executed yet executing")
+            await commit(msg,db,msgn+1,og)
             msgn+=1
         # ja foi executado, so é preciso dar commit
         else:
-            await only_commit(result, db)
+            logging.info("the message has been executed, goint to commit")
+            await only_commit(result, db, msg, msgn+1,og)
             msgn+=1 
+        logging.info("unlocking the cent lock")
+        lock.release()
         await trycommits(msgQ,db)
     else:
         # vamos ver oqq pode ser ja execurado
         await exec_ahead(msgQ,db)
+        logging.info("unlocking the cent lock")
+        lock.release()
 
 #executa todos os comandos que conseguir
 async def exec_ahead(msgQ,db):
     ks = []
-    for n,(msg,og,exe) in msgQ.items():
+    # lock.acquire()
+    for n,(msg,og,exe) in dict(msgQ).items():
         #se ja estiver executado, adiciona as chaves alteradas ao resultado
         if exe:
             ks.extend(exe[1])
@@ -160,7 +186,9 @@ async def exec_ahead(msgQ,db):
                 #pode: executa e adiciona as chaves a que se alterou o valor a ks
                 result = await execute(msg, db)
                 msgQ[n]=(msg,og,result)
-                ks.extend(result[1])
+                #so adicionamos as chaves se a execucao foi bem sucedida
+                if result:
+                    ks.extend(result[1])
             else:
                 #nao pode: adiciona todas as keys dentro do txn ao ks
                 ks.extend([ k for op,k,v in msg.body.txn])
@@ -173,6 +201,8 @@ async def handle(msg):
     # State
     global node_id, node_ids
     global db
+    global lock
+    
 
     # Message handlers
     if msg.body.type == 'init':
@@ -183,7 +213,8 @@ async def handle(msg):
         reply(msg, type='init_ok') 
 
     elif msg.body.type == 'txn':
-        #asks for the msg number
+        printState()
+        #asks for# the msg number
         send(node_id,'lin-tso',type='ts')
         #adds the command to the queue
         queue.append(msg)
@@ -191,11 +222,17 @@ async def handle(msg):
     #replication msg or the commands
     #ads command to q then tries to execute 
     elif msg.body.type == 'txnRep':
+        printState()
         #added msg to the Q
+        #we need the lock because we cant add a message to the que while some msg is being processed
+        await lock.acquire()
         msgQ[msg.body.ts] = (msg.body.msg, node_id==msg.src, None)
+        logging.info("unlocking the cent lock")
+        lock.release()
         await trycommits(msgQ, db)
     
     elif msg.body.type == 'ts_ok':
+        printState()
         #for node in filter(lambda n: n!=node_id,node_ids)
         m = queue.pop(0)
         for node in node_ids:
